77dd54cbade2cde6fe99fbd149ae2e7e16f4b346
Matheus Giehl <giehlmatheus96@gmail.com>
2026-07-14 22:01:24 -0300

    ´Controlando aparencias CSS via JS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+//Trocar a cor do texto da div com a classe teste para azul
 document.querySelector('.teste').style.color = 'blue'
+
+//Adicionar a classe titulo ao h1 e trocar a cor do texto para vermelho
+document.querySelector('h1').classList.add('titulo')
+document.querySelector('.titulo').style.color = 'red'
